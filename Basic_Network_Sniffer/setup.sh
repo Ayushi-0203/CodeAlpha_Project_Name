85f1a06f152a765d2b307e85e36e9834dc712f9e
Ayushi-0203 <122150085+Ayushi-0203@users.noreply.github.com>
2024-12-07 22:57:04 +0530

    Update setup.sh

--- Basic_Network_Sniffer/setup.sh
+++ Basic_Network_Sniffer/setup.sh
@@ -5,6 +5,9 @@ python3 -m venv sniffer
 # activate the virtual environment
 source ./sniffer/bin/activate
 
+# install scapy library 
+pip3 install scapy 
+
 # run the python file with root privilige
 sudo su
 python3 sniffer.py
